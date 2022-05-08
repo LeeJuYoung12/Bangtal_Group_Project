627402b29e22d524db0e326646299acdab8c080e
LeeJuYoung12 <rkfmgm@naver.com>
2022-05-08 23:36:03 +0900

    game1 완성

---
+++ bangtal_group_project/game2.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <bangtal.h>
+
+extern ObjectID createObject(const char* name, SceneID scene, int x, int y, bool shown, float size);
+extern int nowgamenum;
+
+
+
+// 마우스콜백함수
+void game2_mouseCallback(ObjectID object, int x, int y, MouseAction action) {
+
+
+}
+
+//타이머콜백함수
+void game2_timerCallback(TimerID timer) {
+
+}
+
+//사운드콜백함수
+void game2_soundCallback(SoundID sound) {
+
+}
+
+//키보드콜백함수
+void game2_keyboardCallback(KeyCode code, KeyState state) {
+
+}
+
+void game2_main() {
+
+}
