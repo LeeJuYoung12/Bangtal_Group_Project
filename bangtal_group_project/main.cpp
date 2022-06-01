59302ca42bbb84f82bcba910312ad873b723f0a1
LeeJuYoung12 <rkfmgm@naver.com>
2022-06-02 02:32:53 +0900

    main 오류 수정

--- bangtal_group_project/main.cpp
+++ bangtal_group_project/main.cpp
@@ -1,11 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <bangtal.h>
 
-//외부변수 선언
+
 extern SceneID lobbyscene;
 
 
-//외부함수 선언
 extern void lobby_main();
 extern void lobby_mouseCallback(ObjectID object, int x, int y, MouseAction action);
 extern void lobby_timerCallback(TimerID timer);
@@ -43,7 +42,6 @@ extern void game5_soundCallback(SoundID sound);
 extern void game5_keyboardCallback(KeyCode code, KeyState state);
 
 
-//물체 생성함수
 ObjectID createObject(const char* name, SceneID scene, int x, int y, bool shown, float size) {
 	ObjectID object = createObject(name);
 	locateObject(object, scene, x, y);
@@ -54,7 +52,7 @@ ObjectID createObject(const char* name, SceneID scene, int x, int y, bool shown,
 	return object;
 }
 
-// 마우스콜백함수
+
 void mouseCallback(ObjectID object, int x, int y, MouseAction action) {
 
 	lobby_mouseCallback(object, x, y, action);
@@ -65,7 +63,7 @@ void mouseCallback(ObjectID object, int x, int y, MouseAction action) {
 	game5_mouseCallback(object, x, y, action);
 }
 
-//타이머콜백함수
+
 void timerCallback(TimerID timer) {
 
 	lobby_timerCallback(timer);
@@ -76,7 +74,7 @@ void timerCallback(TimerID timer) {
 	game5_timerCallback(timer);
 }
 
-//사운드콜백함수
+
 void soundCallback(SoundID sound) {
 
 	lobby_soundCallback(sound);
@@ -87,7 +85,7 @@ void soundCallback(SoundID sound) {
 	game5_soundCallback(sound);
 }
 
-//키보드콜백함수
+
 void keyboardCallback(KeyCode code, KeyState state) {
 
 	lobby_keyboardCallback(code, state);
